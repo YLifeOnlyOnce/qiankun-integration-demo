74f623bf411e8b6bf4619c6bbb3eedc82477ea17
YLifeOnlyOnce <516294835@qq.com>
2025-07-30 16:40:23 +0800

    Update submodules

--- container/src/qiankun/index.js
+++ container/src/qiankun/index.js
@@ -3,20 +3,20 @@ import { registerMicroApps, start } from "qiankun";
 registerMicroApps(
   [
     {
-      name: 'vite-vue-with-qp',
-      entry: 'http://localhost:5174/',
+      name: 'qiankun-vite-vue',
+      entry: 'http://localhost:5174/microapps/qiankun-vite-vue',
       container: '#micro-container', // 微应用挂载的节点
-      activeRule: '/vite-vue-with-qp',
+      activeRule: '/qiankun-vite-vue',
       props:{
         msg: 'container inject'
       }
     },
     {
-      name: 'webpack-vue-with-qp',
+      name: 'qiankun-webpack-vue',
       // entry : http://xxxx/microapp/name  -> nginx proxy -> 微服务独立部署并启动的地址 http://localhost:5175/
-      entry: 'http://localhost:5175/',
+      entry: 'http://localhost:5175/microapps/qiankun-webpack-vue',
       container: '#micro-container', // 微应用挂载的节点
-      activeRule: '/webpack-vue-with-qp',
+      activeRule: '/qiankun-webpack-vue',
       props:{
         msg: 'container inject'
       }
